955df37ef6188d4f41f8619b05b034cfdc3f8170
Manuel Astudillo <manuel@jacaras.com>
2009-07-27 12:54:08 +0200

    Added convenient methods to the application delegate.

--- docpadAppDelegate.py
+++ docpadAppDelegate.py
@@ -3,12 +3,34 @@
 #  docpad
 #
 #  Created by Manuel Astudillo on 7/26/09.
-#  Copyright __MyCompanyName__ 2009. All rights reserved.
+#  Copyright CodeTonic 2009. All rights reserved.
 #
 
 from Foundation import *
 from AppKit import *
+import objc
 
 class docpadAppDelegate(NSObject):
-    def applicationDidFinishLaunching_(self, sender):
-        NSLog("Application did finish launching.")
+	textView = objc.IBOutlet()
+	
+	def applicationDidFinishLaunching_(self, sender):
+		font = NSFont.fontWithName_size_("Courier", 12)
+		self.textView.setFont_(font)
+		self.textView.setString_("")
+		NSLog("Application did finish launching.")
+		
+	def applicationWillTerminate_(self,sender):
+		NSLog("Application will terminate.")
+		
+	def applicationSupportFolder(self):
+		paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,NSUserDomainMask,True)
+		basePath = (len(paths) > 0 and paths[0]) or NSTemporaryDirectory()
+		fullPath = basePath.stringByAppendingPathComponent_("MetaWindow")
+		if not os.path.exists(fullPath):
+			os.mkdir(fullPath)
+		return fullPath
+        
+	def pathForFilename(self,filename):
+		return self.applicationSupportFolder().stringByAppendingPathComponent_(filename)
+		
+	
